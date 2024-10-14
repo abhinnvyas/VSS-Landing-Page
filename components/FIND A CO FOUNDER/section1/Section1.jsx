19ff26622a67bf83fd19e538c0ad44b75c759dbf
abhinn2@github <abhinnvyas2@gmail.com>
2024-10-15 00:32:25 +0530

    FIXING A DEPLOYMENT ISSUE

--- components/FIND A CO FOUNDER/section1/Section1.jsx
+++ components/FIND A CO FOUNDER/section1/Section1.jsx
@@ -5,7 +5,7 @@ const Section1 = () => {
     <section className="bg-background text-black w-full">
       <div className=" p-10 md:p-14 lg:p-20 w-full flex flex-col items-center justify-center ">
         <h1 className="font-[600] text-2xl lg:text-3xl text-center max-w-4xl">
-          We know even the best founders don't always have people in their
+          We know even the best founders don{"'"}t always have people in their
           network who are ready to start a company
         </h1>
         <p className="font-[600] text-[#6C7C88] text-xl mt-4 ">
